dc3f14eeb2778adfbf7b96f7e610d16e06e32cdd
student-01-e9f448792775 <student-01-e9f448792775@qwiklabs.net>
2021-10-25 22:50:49 +1100

    Move some implementation into AdvancedUIKit

--- iScanner/Entity/CreditCardInfo.swift
+++ iScanner/Entity/CreditCardInfo.swift
@@ -1,24 +1,29 @@
-//
-//  CreditCardInfo.swift
-//  iScanner
-//
-//  Created by Adamas Zhu on 14/9/21.
-//
-
-import Foundation
-
+/// Defines the information of a credit card
+///
+/// - version: 0.1.0
+/// - date: 11/10/21
+/// - author: Adamas
 public struct CreditCardInfo {
 
+    /// The card number
     public let number: String
+
+    /// The expiry date
     public let expiry: Date?
 
+    /// Initialize the object
+    /// - Parameters:
+    ///   - number: The card number
+    ///   - expiry: The expiry date
     public init(number: String,
                 expiry: Date?) {
         self.number = number
         self.expiry = expiry
     }
 
-    init(cardIOCreditCardInfo :CardIOCreditCardInfo) {
+    /// Initialize the object using a card IO object
+    /// - Parameter cardIOCreditCardInfo: The card IO object
+    init(cardIOCreditCardInfo: CardIOCreditCardInfo) {
         number = cardIOCreditCardInfo.cardNumber
         if cardIOCreditCardInfo.expiryYear != 0,
            cardIOCreditCardInfo.expiryMonth != 0 {
@@ -30,3 +35,5 @@ public struct CreditCardInfo {
         }
     }
 }
+
+import Foundation
---
+++ iScanner/Entity/InfoType.swift
@@ -0,0 +1,8 @@
+/// The type of the info to be scanned
+///
+/// - version: 0.1.0
+/// - date: 11/10/21
+/// - author: Adamas
+public enum InfoType {
+    case creditCard
+}
