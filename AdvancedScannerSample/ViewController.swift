74f67a8615d1f8330c360c7f52b0cecfccda53d8
Adamas Zhu <adamas.zhu@coles.com.au>
2023-08-22 17:38:58 +1000

    Build the simple example

--- AdvancedScannerSample/ViewController.swift
+++ AdvancedScannerSample/ViewController.swift
@@ -1,13 +1,23 @@
 final class ViewController: UIViewController {
     
     private lazy var scanner = TextScanner<PriceTagInfo, ScanMode>(viewController: self)
+
+    private lazy var imagePicker: ImagePickerHelper = {
+        let imagePicker = ImagePickerHelper()
+        imagePicker.delegate = self
+        return imagePicker
+    }()
     
     @IBAction private func scan(_ sender: Any) {
         scanner.scan(withHint: "Hold the price tag inside the frame.\nIt will be scanned automatically.") { [weak self] info in
             self?.show(info)
         }
     }
-    
+
+    @IBAction func selectPhoto(_ sender: Any) {
+        imagePicker.showImagePicker()
+    }
+
     private func show(_ info: CreditCardInfo) {
         var message = "Number: \(info.number)"
         if let expiry = info.expiry {
@@ -35,7 +45,31 @@ final class ViewController: UIViewController {
     }
 }
 
+extension ViewController: ImagePickerHelperDelegate {
+
+    func imagePickerHelper(_ imagePickerHelper: ImagePickerHelper,
+                           didPick image: UIImage) {
+        let detector = TextDetector(textTypes: ScanMode.priceTag.textFormats)
+        guard let image = CIImage(image: image) else {
+            return
+        }
+        let detections = detector.detect(image, withLanguageCorrection: true)
+        let description = detections
+            .map(\.string)
+            .joined(separator: .linebreak)
+        Logger.standard.logInfo("Detections", withDetail: description)
+        guard let priceTag = PriceTagInfo(textDetections: detections) else {
+            return
+        }
+        show(priceTag)
+    }
+
+    func imagePickerHelper(_ imagePickerHelper: ImagePickerHelper,
+                           didCatchError error: String) {}
+}
+
 import UIKit
 import AdvancedScanner
 import AdvancedUIKit
+import AdvancedUIKitPhoto
 import AdvancedFoundation
